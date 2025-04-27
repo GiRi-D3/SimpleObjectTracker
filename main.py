450048ba739f7dce9d4b783ca7d6e48c0cfa72ca
GiRi-D3 <dwaipayangiri3@gmail.com>
2025-04-27 05:39:16 +0530

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dataset import TrackingNetDataset
 from model import SimpleObjectTracker
 from train import train_SimpleObjectTracker
 from utils import denormalize
+from config import MODEL_CONFIG, TRAIN_CONFIG
 
 import os
 import torch
@@ -12,5 +13,14 @@ if not os.path.exists(root_dir):
     raise FileNotFoundError(f"Directory '{root_dir}' does not exist.")
 
 dataset = TrackingNetDataset(root_dir=root_dir)
+
 model = SimpleObjectTracker()
-train_SimpleObjectTracker(model, dataset, batch_size=64, num_epochs=50, learning_rate=0.001, save_path='simple_tracker.pth')
+
+train_SimpleObjectTracker(
+    model, 
+    dataset,
+    batch_size=TRAIN_CONFIG['batch_size'],
+    num_epochs=TRAIN_CONFIG['num_epochs'],
+    learning_rate=TRAIN_CONFIG['learning_rate'],
+    save_path=TRAIN_CONFIG['save_path']
+)
